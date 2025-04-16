06a548a69ac959259c61a079d040ef6e24dcc4ae
qwrtsdev <potatoloadzch@gmail.com>
2025-04-17 01:51:46 +0700

    feat: add Vercel Analytics and update some messages

--- portfolio/src/app/layout.jsx
+++ portfolio/src/app/layout.jsx
@@ -1,6 +1,7 @@
 import localFont from "next/font/local";
 import "./globals.css";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 
 const LineSeed = localFont({
     src: [
@@ -58,6 +59,7 @@ export default function RootLayout({ children }) {
                 <div className="scrollbar-hide h-full w-full overflow-auto">
                     {children}
                     <SpeedInsights />
+                    <Analytics />
                 </div>
             </body>
         </html>
