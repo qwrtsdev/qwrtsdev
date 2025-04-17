a6fa7befac3b32798ebaaa962224d74ffd782b69
qwrtsdev <potatoloadzch@gmail.com>
2025-04-18 04:56:45 +0700

    chores: fixed opengraph

--- portfolio/src/app/layout.jsx
+++ portfolio/src/app/layout.jsx
@@ -30,10 +30,10 @@ export const metadata = {
         description:
             "A passionate programmer and graphic designer. Who wants to create well-crafted softwares that seamlessly blends functionality with aesthetics.",
         url: "https://qwrtsdev.xyz/",
-        siteName: "Nuttapong Nakawirot",
+        siteName: "QWRTSDEV",
         images: [
             {
-                url: "https://cdn.discordapp.com/attachments/1297492873398452267/1337792308757794907/qw-opengraph.png?ex=67ee9b2d&is=67ed49ad&hm=642986e7b228cf708dbb8a8dbe5374887d893bc9f122f626656043469ea3f8de&",
+                url: "https://qwrtsdev.xyz/qw-opengraph.png",
                 width: 2048,
                 height: 1070,
             },
@@ -46,7 +46,10 @@ export const metadata = {
         title: "QWRTSDEV",
         description:
             "A passionate programmer and graphic designer. Who wants to create well-crafted softwares that seamlessly blends functionality with aesthetics.",
-        image: "https://cdn.discordapp.com/attachments/1297492873398452267/1337792308757794907/qw-opengraph.png?ex=67ee9b2d&is=67ed49ad&hm=642986e7b228cf708dbb8a8dbe5374887d893bc9f122f626656043469ea3f8de&",
+        image: "https://qwrtsdev.xyz/qw-opengraph.png",
+    },
+    other: {
+        "fb:app_id": "1431782707788084",
     },
 };
 
